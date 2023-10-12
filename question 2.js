71d39944f35871aeb50c026b763bbf7555340ffb
Momodayoo <msyauqi94@gmail.com>
2023-10-12 20:45:30 +0800

    commit on main

--- question 2.js
+++ question 2.js
@@ -5,13 +5,38 @@ b) Write another variant of the truncate function that uses a conditional operat
 console.log(truncate(''This text will be truncated if it is too long', 25))
 // This text will be truncat... */
 
-function truncate(str,max)
+function truncate(str, max, sep){
+    // default to 25 characters
+    max = max || 25; 
 
-{
-    if (max) === 25
-    return truncate
+    var len = str.length; 
+    if (len > max){
+
+        // default to elipsis
+        sep = sep || "...";
+
+        var seplen = sep.length; 
+
+        // if seperator is larger than character limit, right hand side of string will be shown 
+        if (seplen > max){
+            return str.substr (len - max);
+        }
+        // Half the difference between max and string length.
+        // Multiply negative because small minus big.
+        // Must account for length of separator too.
+        var n = -0.005 * (max - len - seplen);
+
+        // This gives us the centerline.
+        var center = len/2;
+
+        var front = str.substr(0, center - n);
+        
+
+        return front + sep;
+
+    }
+
+    return str;
 }
-{
-    
-}
-console.log (truncate('This text will be truncated if it is too long', 25))
+
+console.log(truncate('This text will be truncated if it is too long', 25))
--- question 3.js
+++ question 3.js
@@ -8,9 +8,7 @@ d) Write a function replaceMiddleAnimal(newValue) that replaces the value in the
 middle of the animals array with newValue
 e) Write a function findMatchingAnimals(beginsWith) that returns a new array
 containing all the animals that begin with the beginsWith string. Try to make it work
-regardless of upper/lower case.
-const animals = ['Tiger', 'Giraffe']
-console.log(animals) */
+regardless of upper/lower case. */
 
 const animals = ['Tiger', 'Giraffe']
 console.log(animals)
@@ -21,6 +19,41 @@ console.log (animals)
 animals.unshift ('Snake, Lion')
 console.log (animals)
 //c
-{const animals2 = [ 'Snake, Lion', 'Tiger', 'Giraffe', 'Whale, Dolphin' ]
-console.log (animals2.sort() ) } //<-- check this one
+
+let animal = [ 'Snake', 'Lion', 'Tiger', 'Giraffe', 'Whale', 'Dolphin' ]
+animal.sort (function (a, b) {
+    return a.length - b.length;
+});
+console.log( animal);
 //d 
+function replaceMiddleAnimal(newValue) {
+
+    const list = [
+        "Snake",
+        "Lion",
+        "Tiger",
+        "Giraffe",
+        'Whale',
+        "Dolphin"
+    ];
+
+    list.splice (2, 1, "Turtle");
+
+    console.log (list);
+}
+replaceMiddleAnimal();
+
+//e  
+
+function findMatchingAnimal(beginsWith) {
+    
+    const list = [
+        { id: 1, Animal: 'Snake', type: 'reptile', limb: 0 },
+        { id: 2, Animal: 'Lion', type: 'mammal', limb: 4 }, 
+        { id: 3, Animal: 'Tiger', type: 'mammal', limb: 4 },
+        { id: 4, Animal: 'Giraffe', type: 'mammal', limb: 4},
+        { id: 5, Animal: 'Whale', type: 'mammal', limb: 0},
+        { id: 6, Animal: 'Dolphin', type: 'mammal', limb: 0}]
+    }
+       const object = list.find (obj => obj.id === 3);
+console.log (object)
